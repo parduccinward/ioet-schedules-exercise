f21a5a96a0810274ca82e3b54b5bc8e7a4052a93
Diego <dpardoblacutt@gmail.com>
2022-10-31 18:37:34 -0400

    Adding pair of tests to calculateCoincidence

--- src/tests/calculateCoincidences.test.js
+++ src/tests/calculateCoincidences.test.js
@@ -50,6 +50,46 @@ const astridScheduleEx2 = {
     SU: { start: '20:00', end: '21:00' }
 }
 
+const oneMinTestSchedule1 = {
+    MO: { start: '07:59', end: '08:00' },
+    TU: { start: '', end: '' },
+    WE: { start: '', end: '' },
+    TH: { start: '', end: '' },
+    FR: { start: '', end: '' },
+    SA: { start: '', end: '' },
+    SU: { start: '', end: '' }
+}
+
+const oneMinTestSchedule2 = {
+    MO: { start: '06:00', end: '09:00' },
+    TU: { start: '', end: '' },
+    WE: { start: '', end: '' },
+    TH: { start: '', end: '' },
+    FR: { start: '', end: '' },
+    SA: { start: '', end: '' },
+    SU: { start: '', end: '' }
+}
+
+const exclusiveTestSchedule1 = {
+    MO: { start: '08:00', end: '10:00' },
+    TU: { start: '', end: '' },
+    WE: { start: '', end: '' },
+    TH: { start: '', end: '' },
+    FR: { start: '', end: '' },
+    SA: { start: '', end: '' },
+    SU: { start: '', end: '' }
+}
+
+const exclusiveTestSchedule2 = {
+    MO: { start: '10:00', end: '12:00' },
+    TU: { start: '', end: '' },
+    WE: { start: '', end: '' },
+    TH: { start: '', end: '' },
+    FR: { start: '', end: '' },
+    SA: { start: '', end: '' },
+    SU: { start: '', end: '' }
+}
+
 describe('Calculate coincidences between a couple of working schedules', () => {
     test("Astrid-Rene test Ex:1", () => {
         expect(serviceUtils.calculateCoincidences(astridScheduleEx1,reneScheduleEx1)).toBe(2);
@@ -62,6 +102,12 @@ describe('Calculate coincidences between a couple of working schedules', () => {
     })
     test("Astrid-Rene test Ex:2", () => {
         expect(serviceUtils.calculateCoincidences(astridScheduleEx2,reneScheduleEx2)).toBe(3);
+    })
+    test("Coincidence by one minute", () => {
+        expect(serviceUtils.calculateCoincidences(oneMinTestSchedule1,oneMinTestSchedule2)).toBe(1);
+    })
+    test("Exclusive coincidence, one arriving the other lefting", () => {
+        expect(serviceUtils.calculateCoincidences(exclusiveTestSchedule1,exclusiveTestSchedule2)).toBe(0);
     })
 })
 
